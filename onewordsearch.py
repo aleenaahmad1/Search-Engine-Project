560083b3c1d001a646568b847557427f94a93be6
Uneeb29 <116509560+Uneeb29@users.noreply.github.com>
2022-12-29 21:21:21 +0500

    Update onewordsearch.py

--- onewordsearch.py
+++ onewordsearch.py
@@ -3,6 +3,10 @@ import re
 import math
 import time
 from nltk.stem.snowball import SnowballStemmer
+from tkinter import *
+from tkinter import ttk
+import shutil
+
 
 
 snow_stemmer = SnowballStemmer(language='english')
@@ -30,6 +34,9 @@ file4 = open("E:\indexfilesfinal\invertedIndexTitleFinal.json")
 titleinv = json.load(file4)
 
 docScores = {}
+
+resultsFound = "Results Found: "
+resultList = []
 
 
 def titlesearch(wordlist):
@@ -65,11 +72,15 @@ def search(input):
     sortingdocs()
 
 def sortingdocs(): #sorting docs for multiple word query
+    global resultsFound
     finaldocs = sorted(docScores.items(), key=lambda i:i[1], reverse=True)
-    print(len(finaldocs), "results found")
+    resultsFound += str(len(finaldocs))
+    resultList.append(resultsFound)
     for i in finaldocs:
         print(docIndex.get(str(i[0]))[1])
+        resultList.append(docIndex.get(str(i[0]))[1])
         print(docIndex.get(str(i[0]))[0])
+        resultList.append(docIndex.get(str(i[0]))[0])
         print("\n")
 
 def getRank(doclist, ID):
@@ -86,6 +97,85 @@ def getRank(doclist, ID):
         else:
             docScores.update({i:tfidf})
 
+window = Tk()
+
+
+window.title("GBW")
+
+
+window.geometry("800x600+200+100")
+
+
+bg = PhotoImage(file="photo.png")
+bglbl = Label(window, image=bg)
+bglbl.place(x=0, y=0)
+
+
+queryText = ""
+newDoc = ""
+
+
+def get_query():
+    newWindow = Tk()
+    newWindow.title("Search Results")
+    newWindow.geometry("1280x1000")
+
+    tempStr = ''
+    urlLabel = Label(newWindow,height=100,width=200,background="light blue", anchor='nw', font="TimesNewRoman 20")
+    urlLabel.place(x=0,y=0)
+    queryText = entrywidget.get()
+    search(queryText)
+
+    for i in range (len(resultList)):
+        tempStr+= resultList[i] + '\n'
+        if i%2 == 0:
+            tempStr += '\n''\n'
+
+    urlLabel.config(text = tempStr)
+
+def moveDoc(entry):
+    newDoc = entry.get()
+    destinationPath = "E:\sample1"
+    shutil.move(newDoc,destinationPath)
+
+def addNewDoc():
+    aNewWindow = Tk()
+
+
+    aNewWindow.title("Add Document")
+
+
+    aNewWindow.geometry("800x600+200+100")
+
+
+    entry1 = ttk.Entry(aNewWindow, width=50)
+    entry1.pack(pady=10)
+
+
+    btn2 = ttk.Button(aNewWindow, text="Add", command=lambda: moveDoc(entry1))
+    btn2.pack(pady=10)
+
+
+    aNewWindow.mainloop()
+
+
+inputText = StringVar()
+
+mylabel = Label(window, text="GBW", font="TimesNewRoman 20", foreground="orange", background="light blue")
+mylabel.pack(pady=10)
+
+entrywidget = ttk.Entry(window, textvariable=inputText, width=50)
+entrywidget.pack(pady=10)
+
+
+mybtn = ttk.Button(window, text="Search", command=get_query)
+mybtn.pack(pady=10)
+
+btn1 = ttk.Button(window, text="Add A New Document", command=addNewDoc)
+btn1.pack(pady=10)
+
+
+window.mainloop()
+
 start = time.time()    
-search("usa afghanistan")
 print("time:", time.time()-start)
